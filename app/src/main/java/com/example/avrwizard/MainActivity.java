0b6d630dd78154a819352c90811e0a97d85e5e3d
Muh. Shofuwan Anwar <hadoepan@gmail.com>
2020-11-21 13:12:06 +0700

    screen size

--- app/src/main/java/com/example/avrwizard/MainActivity.java
+++ app/src/main/java/com/example/avrwizard/MainActivity.java
@@ -16,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+
         Button btnHelpMe = (Button) findViewById(R.id.btnHelpMe);
         btnHelpMe.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/avrwizard/selectedAdvanced.java
+++ app/src/main/java/com/example/avrwizard/selectedAdvanced.java
@@ -3,6 +3,7 @@ package com.example.avrwizard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class selectedAdvanced extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class selectedAdvanced extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_advanced);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
     }
 }
