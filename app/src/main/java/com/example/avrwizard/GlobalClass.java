c36e44e3b0837b3ed7836df3650a068068025f2e
mukhlisdewe <45230164+mukhlisdewe@users.noreply.github.com>
2020-12-01 23:27:14 +0700

    JSONEngine
    JSON yang belum fix

--- app/src/main/java/com/example/avrwizard/GlobalClass.java
+++ app/src/main/java/com/example/avrwizard/GlobalClass.java
@@ -1,4 +1,33 @@
 package com.example.avrwizard;
 
 public class GlobalClass {
+    int flash;
+    int sram;
+    int eeprom;
+
+    public int getFlash() {
+        return flash;
+    }
+
+    public void setFlash(int flash) {
+        this.flash = flash;
+    }
+
+    public int getSram() {
+        return sram;
+    }
+
+    public void setSram(int sram) {
+        this.sram = sram;
+    }
+
+    public int getEeprom() {
+        return eeprom;
+    }
+
+    public void setEeprom(int eeprom) {
+        this.eeprom = eeprom;
+    }
+
+
 }
