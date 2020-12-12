30102f5caf4cf0cdebbc21214cb2a17215c45d14
Muh. Shofuwan Anwar <hadoepan@gmail.com>
2020-12-12 14:26:32 +0700

    code generator activity by shofuwan

---
+++ app/src/main/java/com/example/avrwizard/GeneratedProgram.java
@@ -0,0 +1,14 @@
+package com.example.avrwizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneratedProgram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generated_program);
+    }
+}
